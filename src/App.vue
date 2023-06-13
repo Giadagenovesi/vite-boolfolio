fc930a454c825ee00d146d0327e967fec0d8204d
Giadagenovesi <giada.g@outlook.it>
2023-06-13 17:15:08 +0200

    Add cards in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
-<script></script>
+<script>
+import axios from 'axios';
 
-<template></template>
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      console.log('ecco i miei project')
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        this.projects = resp.data.results; 
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container py-5">
+    <h2>I miei Progetti</h2>
+    <div class="row row-cols-3">
+      <div class="col g-4" v-for="project in projects" :key="projects.id">
+        <div class="card">
+          <h5>{{ project.title }}</h5>
+          <p>{{ project.content }}</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
+
+.card {
+  height: 100%;
+}
 </style>
