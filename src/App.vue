cfa903c958a670ca50b2e5606cacb5e4cd538cf2
Giadagenovesi <giada.g@outlook.it>
2023-06-14 18:34:50 +0200

    Completed Bonus 1

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,28 @@ export default {
   data() {
     return {
       projects: [],
-      store
+      store,
+      currentPage: 1,
+      lastPage: null,
+      totalProjects: 0,
     }
   },
   mounted() {
     this.getProjects();
   },
   methods: {
-    getProjects() {
+    getProjects(pageNumber = 1) {
       console.log('ecco i miei project')
-      axios.get(`${store.apiUrl}/api/projects`).then(resp => {
-        this.projects = resp.data.results; 
+      axios.get(`${store.apiUrl}/api/projects`, {
+        params: {
+          page:pageNumber
+        }
+      }).then(resp => {
+        console.log(resp);
+        this.projects = resp.data.results.data;
+        this.currentPage = resp.data.results.current_page;
+        this.lastPage = resp.data.results.last_page;
+        this.totalProjects = resp.data.results.total;
       });
     }
   },
@@ -33,6 +44,42 @@ export default {
         <ProjectCard :project="project" />
       </div>
     </div>
+
+    <!-- Pagination -->
+
+    <nav
+      v-if="lastPage"
+      aria-label="Page navigation example"
+      class="my-3 d-flex justify-content-end"
+    >
+      <ul class="pagination">
+        <li class="page-item" :class="{ disabled: currentPage === 1 }">
+          <a
+            @click.prevent="getProjects(currentPage - 1)"
+            class="page-link"
+            href="#"
+            >Previous</a
+          >
+        </li>
+        <li
+          class="page-item"
+          :class="{ active: pageNum === currentPage }"
+          v-for="pageNum in lastPage"
+        >
+          <a @click.prevent="getProjects(pageNum)" class="page-link" href="#">{{
+            pageNum
+          }}</a>
+        </li>
+        <li class="page-item" :class="{ disabled: currentPage === lastPage }">
+          <a
+            @click.prevent="getProjects(currentPage + 1)"
+            class="page-link"
+            href="#"
+            >Next</a
+          >
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
