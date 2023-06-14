3ee11919e378539c346cf29b075f25c4288313bc
Giadagenovesi <giada.g@outlook.it>
2023-06-14 18:48:58 +0200

    Completed Bonus 2

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,9 +15,21 @@ export default {
 </script>
 
 <template>
-    <div class="card">
+    <div class="card p-2">
           <h5>{{ project.title }}</h5>
           <p>{{ project.content }}</p>
+          <p class="text-primary">
+            <ul class="list-unstyled" v-if="project.technologies.length">
+                <li v-for="technology in project.technologies" :key="technology.id">
+                    {{ technology.name }}
+                </li>
+            </ul>
+            <span v-else>Nessuna tecnologia</span>
+          </p>
+          <p>
+            <span v-if="project.type"> {{ project.type.name }}</span>
+            <span v-else>Nessuna tipologia</span>
+          </p>
         </div>
 </template>
 
